f3f8ecb79293155df6e9669babbc150c00351b85
LeoCreerIII <leo.creer@gmail.com>
2021-11-04 16:17:53 -0700

    Added Service for on resize logic. Fix an issue that was causing app.isStable to always be false in applications.

--- projects/ngx-focus-point/src/public_api.ts
+++ projects/ngx-focus-point/src/public_api.ts
@@ -3,6 +3,7 @@
  */
 export  * from './lib/on-resize';
 export  * from './lib/position.model';
+export  * from './lib/on-resize.service';
 export  * from './lib/ngx-focus-point-select.component';
 export * from './lib/ngx-focus-point.component';
 export * from './lib/ngx-focus-point.module';
